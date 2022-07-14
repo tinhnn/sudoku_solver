03c7cd869cab5c0403ae4e584e52b53025248598
Nguyen Nhan Tinh <nguyennhantinh@outlook.com>
2022-07-14 23:47:07 +0900

    add cs prj

---
+++ cpp/sudoku/sudoku_cs/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace sudoku_cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setBoardRegionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1184, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setBoardRegionToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // setBoardRegionToolStripMenuItem
+            // 
+            this.setBoardRegionToolStripMenuItem.Name = "setBoardRegionToolStripMenuItem";
+            this.setBoardRegionToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.setBoardRegionToolStripMenuItem.Text = "set board region";
+            this.setBoardRegionToolStripMenuItem.Click += new System.EventHandler(this.setBoardRegionToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(903, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(280, 280);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1053, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 936);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sudoku Solver";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setBoardRegionToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ cpp/sudoku/sudoku_cs/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using IronOcr;
+
+namespace sudoku_cs
+{
+    public partial class Form1 : Form
+    {
+        public int[,] _Board = new int[9, 9];
+        public Button[,] buttons = new Button[9, 9];
+        public int button_sz = 100;
+        private int __IMAGE_W = 625;
+        private int __IMAGE_H = 625;
+        private int __BOARD_POS_X = 172;
+        private int __BOARD_POS_Y = 223;
+        public Form1()
+        {
+            InitializeComponent();
+            InitBoard();
+        }
+
+        private void InitBoard()
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    Button button = new Button();
+                    buttons[i, j] = button;
+                    button.Size = new Size(button_sz, button_sz);
+                    button.Text = _Board[i, j].ToString();
+                    button.Font = new Font("Arial", 36F, FontStyle.Bold);
+                    if (((i >= 0 && i <= 2) && (j >= 3 && j <= 5))||
+                        ((i>=3&&i<=5)&&((j>=0&&j<=2)||(j>=6&&j<=8)))||
+                        ((i>=6&&i<=8)&&(j>=3&&j<=5)))
+                    {
+                        button.BackColor = Color.FromArgb(180, 167, 218);
+                    }
+                    else
+                    {
+                        button.BackColor = Color.FromArgb(192, 192, 192);
+                    }
+                    
+                    button.Location = new Point(j * button_sz, i * button_sz + 30);
+                    this.Controls.Add(button);
+                }
+            }
+        }
+
+        private void setBoardRegionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap capBitmap = new Bitmap(__IMAGE_W, __IMAGE_H, PixelFormat.Format32bppArgb);
+            Rectangle capRec = Screen.AllScreens[0].Bounds;
+            Graphics capGrap = Graphics.FromImage(capBitmap);
+            capGrap.CopyFromScreen(__BOARD_POS_X, __BOARD_POS_Y, 0, 0, new Size(__IMAGE_W, __IMAGE_H));
+            pictureBox1.Image = capBitmap;
+            //capBitmap.Save("board.jpg", ImageFormat.Jpeg);
+
+            GetNumberFromImage(capBitmap);
+        }
+        private void GetNumberFromImage(Bitmap img)
+        {
+            var Ocr = new IronTesseract();
+            Ocr.Configuration.PageSegmentationMode = TesseractPageSegmentationMode.SingleChar;
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    int posx = 6 + j * 69;
+                    int posy = 7 + i * 69;
+                    var content = new System.Drawing.Rectangle() { X = posx, Y = posy, Width = 60, Height = 60 };
+                    var Input = new OcrInput();
+                    Input.AddImage(img, content);
+                    var result = Ocr.Read(Input);
+
+                    // update to screen
+                    int n;
+                    Boolean isNum = int.TryParse(result.Text, out n);
+                    if(n < 1 || n > 9)
+                    {
+                        _Board[i, j] = 0;
+                        buttons[i, j].Text = "0";
+                    }
+                    else
+                    {
+                        _Board[i, j] = n;
+                        buttons[i, j].Text = n.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
